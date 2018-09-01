7aaed4be81a5648a2cd415b21ac70ec27dc3a006
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-01 09:55:19 -0400

    #14: analysis.R, add initial file

---
+++ analysis/analysis.R
@@ -0,0 +1,25 @@
+##
+## arima.R, ARIMA analysis on wikipedia traffic:
+##
+##     - https://www.dropbox.com/s/x14f3bg8flej1n7/train-wikipedia.csv?dl=1
+##     - https://www.dropbox.com/s/o2df10dnyt3bg02/test-wikipedia.csv?dl=1
+##
+
+## set project cwd: only execute in RStudio
+if (nzchar(Sys.getenv('RSTUDIO_USER_IDENTITY'))) {
+  cwd <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+  setwd(cwd)
+}
+
+## load packages
+load_package(c('data.table', 'RJSONIO'))
+
+## utility functions
+devtools::install_local(paste(cwd, sep='', '/packages/customUtility'))
+library('customUtility')
+
+## create dataframes
+df.wikipedia = load_data('data/wikipedia', remove=TRUE, type='json')
+df.twitter = load_data('data/twitter', remove=TRUE, type='json')
+df.ixic = load_data('data/nasdaq/^ixic.csv', remove=TRUE, type='csv')
+df.ndx = load_data('data/nasdaq/^ndx.csv', remove=TRUE, type='csv')
