c7c3402eca9306d9ecbd281c4996a1121e71721a
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-22 16:47:04 -0400

    #22: remove custom text scoring

--- packages/customUtility/R/load_package.R
+++ packages/customUtility/R/load_package.R
@@ -1,8 +1,8 @@
 ##
 ## load_package.R, install + load defined package
 ##
-load_package <- function(packages) {
-  new.packages <- packages[!(packages %in% installed.packages()[, 'Package'])]
+load_package = function(packages) {
+  new.packages = packages[!(packages %in% installed.packages()[, 'Package'])]
   if (length(new.packages))
     install.packages(new.packages, dependencies = TRUE, repos='http://cran.rstudio.com/')
   sapply(packages, require, character.only = TRUE)
