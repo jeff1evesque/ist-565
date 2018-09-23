6390ead8cc2ca08b207a91557e69db791d3aea48
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-23 10:52:50 -0400

    #31: exploratory.R, remove twitter + wikipedia data

--- analysis/exploratory.R
+++ analysis/exploratory.R
@@ -30,56 +30,12 @@ library('customUtility')
 load_package(c('data.table', 'RJSONIO', 'tidytext', 'tidyverse', 'gtools'))
 
 ## create dataframes
-df.twitter = load_data(paste0(cwd, '/data/twitter'), remove=TRUE, type='json')
 df.ixic = load_data(paste0(cwd, '/data/nasdaq/^ixic.csv'), remove=TRUE, type='csv')
-df.ndx = load_data(paste0(cwd, '/data/nasdaq/^ndx.csv'), remove=TRUE, type='csv')
 
 ## remove time from datetime
 df.twitter$timestamp = unlist(lapply(strsplit(
   as.character(df.twitter$timestamp), ' '),
   '[[', 1)
-)
-
-## remove 2000-2013
-df.twitter = subset(
-  df.twitter,
-  !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', timestamp)
-)
-df.ixic = subset(
-  df.ixic,
-  !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', Date)
-)
-df.ndx = subset(
-  df.ndx,
-  !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', Date)
-)
-
-## convert all words
-df.twitter$text = tolower(df.twitter$text)
-
-## apply sentiment analysis
-tweets.unnested = df.twitter %>% unnest_tokens(word, text)
-tweets = tweets.unnested %>%
-  group_by(timestamp) %>%
-  mutate(word_count = 1:n()) %>%
-  inner_join(get_sentiments('bing')) %>%
-  count(timestamp, index = word_count, sentiment) %>%
-  ungroup() %>%
-  spread(sentiment, n, fill = 0) %>%
-  mutate(sentiment = positive - negative)
-
-## tweets: entire plot
-nsize = nrow(tweets)
-ggplot(tweets, aes(index, sentiment, fill = sentiment > 0)) +
-  geom_bar(alpha = 0.5, stat = 'identity', show.legend = FALSE) +
-  facet_wrap(~timestamp, ncol = 15, scales = 'free_x')
-
-## save ggplot
-ggsave(
-  file='visualization/twitter_sentiment.png',
-  width = 24,
-  height = 12,
-  units = 'in'
 )
 
 ## time series: ixic
