e84fd190ce65d9355444cb096c40d8682296220d
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-23 10:30:14 -0400

    #29: exploratory.R, append day of week

--- analysis/exploratory.R
+++ analysis/exploratory.R
@@ -3,7 +3,6 @@
 ##
 ##     - twitter
 ##     - nasdaq (ixic)
-##     - nasdaq (ndx)
 ##
 
 ## set project cwd: only execute in RStudio
@@ -31,43 +30,13 @@ load_package(c('data.table', 'RJSONIO', 'tidytext', 'tidyverse', 'gtools'))
 
 ## create dataframes
 df.wikipedia = load_data(paste0(cwd, '/data/wikipedia'), remove=TRUE, type='json')
-df.twitter = load_data(paste0(cwd, '/data/twitter'), remove=TRUE, type='json')
 df.ixic = load_data(paste0(cwd, '/data/nasdaq/^ixic.csv'), remove=TRUE, type='csv')
-df.ndx = load_data(paste0(cwd, '/data/nasdaq/^ndx.csv'), remove=TRUE, type='csv')
-
-## remove time from datetime
-df.twitter$timestamp = unlist(lapply(strsplit(
-  as.character(df.twitter$timestamp), ' '),
-  '[[', 1)
-)
 
 ## remove 2000-2013
-df.twitter = subset(
-  df.twitter,
-  !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', timestamp)
-)
 df.ixic = subset(
   df.ixic,
   !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', Date)
 )
-df.ndx = subset(
-  df.ndx,
-  !grepl('^(200[0-9]{1}|201[0-2]{1}|2013-[0]{1}[1-9]{1}|2013-[1-9]{1}[0-9]{1})-', Date)
-)
-
-## convert all words
-df.twitter$text = tolower(df.twitter$text)
-
-## apply sentiment analysis
-tweets.unnested = df.twitter %>% unnest_tokens(word, text)
-tweets = tweets.unnested %>%
-  group_by(timestamp) %>%
-  mutate(word_count = 1:n()) %>%
-  inner_join(get_sentiments('bing')) %>%
-  count(timestamp, index = word_count, sentiment) %>%
-  ungroup() %>%
-  spread(sentiment, n, fill = 0) %>%
-  mutate(sentiment = positive - negative)
 
 ## tweets: entire plot
 nsize = nrow(tweets)
@@ -85,9 +54,9 @@ ggsave(
 
 ## time series: ixic
 ggplot(data = df.ixic) +
-  geom_point(aes(Date, as.numeric(Open)), color='red') +
-  geom_point(aes(Date, as.numeric(Close)), color='blue') +
-  labs(x = 'Date', y = 'IXIC Price', title = 'IXIC Price vs Date') +
+  geom_point(aes(Date, as.numeric(High)), color='red') +
+  geom_point(aes(Date, as.numeric(Low)), color='blue') +
+  labs(x = 'Date', y = 'ixic Price', title = 'ixic Price vs Date') +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
@@ -98,39 +67,5 @@ ggsave(
   dpi = 100
 )
 
-## time series: ndx
-ggplot(data = df.ndx) +
-  geom_point(aes(Date, as.numeric(High)), color='red') +
-  geom_point(aes(Date, as.numeric(Low)), color='blue') +
-  labs(x = 'Date', y = 'NDX Price', title = 'NDX Price vs Date') +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-ggsave(
-  'visualization/timeseries-ndx.png',
-  width = 16,
-  height = 9,
-  dpi = 100
-)
-
-## remove day suffix
-tweets$timestamp = unlist(lapply(
-  tweets$timestamp,
-  FUN=function(x)(sub('^(201[0-9]{1}-[0-9]{1}[0-9]{1}).*', '\\1', x))
-))
-df.ixic$Date = unlist(lapply(
-  df.ixic$Date,
-  FUN=function(x)(sub('^(201[0-9]{1}-[0-9]{1}[0-9]{1}).*', '\\1', x))
-))
-df.ndx$Date = unlist(lapply(
-  df.ndx$Date,
-  FUN=function(x)(sub('^(201[0-9]{1}-[0-9]{1}[0-9]{1}).*', '\\1', x))
-))
-
-## aggregate rows
-tweets.agg = aggregate(. ~ timestamp, tweets, sum)
-names(tweets.agg)[names(tweets.agg) == 'timestamp'] = 'Date'
-
-## merge dataframes
-df.ndx.tweets = merge(tweets.agg, df.ndx, all = TRUE)
-df.ixic.tweets = merge(tweets.agg, df.ixic, all = TRUE)
+## day of week
+df.ixic$day = weekdays(as.Date(df.ixic$Date,'%d/%m/%Y'))
